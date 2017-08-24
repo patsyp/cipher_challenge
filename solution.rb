b46b5947f88ff0523c703d060f4afb00a1df3243
Patsy Pineda <patsyp014@gmail.com>
2017-08-24 08:40:32 -0500

    first commit

---
+++ solution.rb
@@ -0,0 +1,53 @@
+
+crypt_file = 'encrypted.txt'
+base = 'plain.txt'
+
+#Placed text for both files into arrays.
+encrypted = File.readlines(crypt_file)
+plain = File.readlines(base)
+
+#Deleted blank lines
+encrypted.delete("\n")
+plain.delete("\n")
+
+#Broke base down into different works by looking for a start and end.
+beggining = 1
+ending = 1
+base_sliced = []
+for i in 0...plain.length
+	if plain[i] == "by William Shakespeare\n"
+		beggining=i-1 #To start at title
+	end
+	if plain[i] == "THE END\n"
+		ending = i 
+		base_sliced.push(plain[beggining..ending])
+	end
+end
+
+#Assuming length of the encrypted array is close to the length of one of the base_sliced sub array lengths. Give or take 100 lines just in case.
+possible_works = []
+decrypted_work = []
+for i in 0...base_sliced.length
+	if base_sliced[i].length <= encrypted.length + 100 and base_sliced[i].length > encrypted.length - 100
+		possible_works.push(base_sliced[i])
+	end
+	#narrowed down to one by comparing length of first two lines
+	if possible_works.length > 1
+		for work in possible_works
+			if work.first.length == encrypted.first.length and work[1].length == encrypted[1].length
+				decrypted_work = work
+			end
+		end
+	elsif possible_works.length == 1
+		decrypted_work = possible_works[0]
+	end
+end
+
+File.open("deciphered.txt", "w+") { |file| file.write(decrypted_work) }
+
+
+
+
+
+
+
